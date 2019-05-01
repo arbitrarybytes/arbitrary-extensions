1537925cde07350791e492353408f68a2e103b0b
arbitrarybytes <arbitrarybytes@users.noreply.github.com>
2019-05-01 12:25:11 +0530

    TypeConversions and Date Time extensions added

---
+++ src/ArbitraryExtensions/DateTimeExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArbitraryExtensions
+{
+    public static class DateTimeExtensions
+    {
+        public static int Age(this DateTime value)
+        {
+            if (DateTime.Today.Month < value.Month ||
+               DateTime.Today.Month == value.Month &&
+                DateTime.Today.Day < value.Day)
+            {
+                return DateTime.Today.Year - value.Year - 1;
+            }
+
+            return DateTime.Today.Year - value.Year;
+        }
+    }
+}

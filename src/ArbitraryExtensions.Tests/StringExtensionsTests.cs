e338e9e5929a57f6e2c586e70dbf61e4a2f40047
Vaibhav Sharma <arbitrarybytes@users.noreply.github.com>
2020-08-09 21:35:05 +0530

    String extensions added

---
+++ src/ArbitraryExtensions.Tests/StringExtensionsTests.cs
@@ -0,0 +1,65 @@
+using System;
+
+using ArbitraryExtensions.Core;
+
+using NUnit.Framework;
+
+namespace ArbitraryExtensions.Tests
+{
+    public class StringExtensionsTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        [TestCase(null, default(string))]
+        [TestCase("", default(string))]
+        [TestCase("Value", "Value")]
+        [TestCase(" ", " ")]
+        public void TestDefaultIfNullOrEmpty(string value, string expect)
+        {
+            Assert.AreEqual(expect, StringExtensions.DefaultIfNullOrEmpty(value));
+        }
+
+        [Test]
+        [TestCase(null, null, null)]
+        [TestCase(null, default(string), default(string))]
+        [TestCase(null, "Default", "Default")]
+        [TestCase("", "Default", "Default")]
+        [TestCase(" ", "Default", " ")]
+        public void TestDefaultIfNullOrEmptyWithDefaultValue(string value, string defaultValue, string expect)
+        {
+            Assert.AreEqual(expect, StringExtensions.DefaultIfNullOrEmpty(value, defaultValue));
+        }
+
+        [Test]
+        [TestCase(null, default(string))]
+        [TestCase("", default(string))]
+        [TestCase("Value", "Value")]
+        [TestCase(" ", default(string))]
+        [TestCase("\n", default(string))]
+        [TestCase("\r", default(string))]
+        [TestCase("\t", default(string))]
+        public void TestDefaultIfNullOrWhitespace(string value, string expect)
+        {
+            Assert.AreEqual(expect, StringExtensions.DefaultIfNullOrWhitespace(value));
+        }
+
+        [Test]
+        [TestCase(null, null, default(string))]
+        [TestCase(null, default(string), default(string))]
+        [TestCase(null, "Default", "Default")]
+        [TestCase("", "Default", "Default")]
+        [TestCase(" ", "Default", "Default")]
+        [TestCase("\r", "Default", "Default")]
+        [TestCase("\n", "Default", "Default")]
+        [TestCase("\t", "Default", "Default")]
+        public void TestDefaultIfNullOrWhitespaceWithDefaultValue(string value, string defaultValue, string expect)
+        {
+            Assert.AreEqual(expect, StringExtensions.DefaultIfNullOrWhitespace(value, defaultValue));
+        }
+
+    }
+}
---
+++ src/ArbitraryExtensions/Core/StringExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ArbitraryExtensions.Core
+{
+    public static class StringExtensions
+    {
+        /// <summary>Checks if the provided input is null or empty and returns the provided default value instead.</summary>
+        /// <param name="input">the value to check</param>
+        /// <param name="defaultValue">the default value to return if input is null or empty, optional.</param>
+        /// <returns>input if it's not null or empty, otherwise the provided default value.</returns>
+        public static string DefaultIfNullOrEmpty(this string input, string defaultValue = default) => string.IsNullOrEmpty(input) ? defaultValue : input;
+
+        /// <summary>Checks if the provided input is null or whitespace and returns the provided default value instead.</summary>
+        /// <param name="input">the value to check</param>
+        /// <param name="defaultValue">optional, the default value to return if input is null or whitepace.</param>
+        /// <returns>input if it's not null or whitespace, otherwise the provided default value.</returns>
+        public static string DefaultIfNullOrWhitespace(this string input, string defaultValue = default) => string.IsNullOrWhiteSpace(input) ? defaultValue : input;
+
+    }
+}
